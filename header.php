6e0bcba04f2541b054adc772bf31800aee3d7d55
Kevan Lee <kevanlee@users.noreply.github.com>
2015-01-24 19:22:01 -0700

    Update header.php

--- header.php
+++ header.php
@@ -42,22 +42,8 @@
 
 
         <!-- Begin MailChimp Signup Form -->
-        <div id="mc_embed_signup">
-          <form action="//kevanlee.us1.list-manage.com/subscribe/post?u=<?php echo get_option( 'text_test' ); ?>" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <div id="mc_embed_signup_scroll">
-              <h2>Subscribe to our mailing list</h2>
-              <div class="mc-field-group">
-                <label for="mce-EMAIL">Email Address </label>
-                <input type="email" value="" name="EMAIL" placeholder="Email Address" class="required email" id="mce-EMAIL"><input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" class="button">
-              </div>
-              <div id="mce-responses" class="clear">
-                <div class="response" id="mce-error-response" style="display:none"></div>
-                <div class="response" id="mce-success-response" style="display:none"></div>
-              </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-              <div style="position: absolute; left: -5000px;"><input type="text" name="b_269b1b68a4034096e04f017a0_299f8ed916" tabindex="-1" value=""></div>
-            </div>
-          </form>
-        </div>
+        <?php echo get_option( 'text_test' ); ?>
+
 
         <!--End mc_embed_signup-->
 
